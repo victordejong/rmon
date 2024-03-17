9f175c37edd2896cd733733c7f24a3a66362a9c4
Victor de Jong <victor@victordejong.com>
2024-03-18 00:08:05 +0100

    Modify main.rs to use the new data structures and collectors

--- client/src/main.rs
+++ client/src/main.rs
@@ -6,24 +6,22 @@ Author: Victor de Jong <victor@victordejong.com>
 
 use chrono::{Local};
 use std::{thread, time};
-use cpu_freq::CpuFreqs;
+
+mod collectors;
+mod data_structs;
 
 const SLEEP_DUR: time::Duration = time::Duration::from_millis(5000);
 
 fn main() {
     println!("Starting RMON-Client on {}", Local::now().format("%Y-%m-%dT%H:%M:%S%Z"));
     println!("Getting CPU info with 5 second intervals...");
+
+    let mut live_metrics: data_structs::LiveMetrics = data_structs::init_live_metrics_struct();
+
     loop {
-        let cpus: Vec<CpuFreqs> = cpu_freq::get();
-        let mut count: f32 = 0.;
-        for (i, e) in cpus.iter().enumerate() {
-            let cpu_freq_temp = match e.cur {
-                None => panic!("No CPU value for {}", i),
-                Some(element) => element,
-            };
-            count += cpu_freq_temp;
-        }
-        println!("Current average frequency: {}", count / cpus.len() as f32);
+        collectors::cpu::collect_cpu_data(&mut live_metrics);
+        
+        println!("CPU: cores: {}, frequency average {} MHz", live_metrics.cpu.amount, live_metrics.cpu.freq);
         thread::sleep(SLEEP_DUR);
     }
 }
