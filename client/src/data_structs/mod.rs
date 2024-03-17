eed45e560f64afd75d3091476aaaacdaadc1ca52
Victor de Jong <victor@victordejong.com>
2024-03-18 00:05:43 +0100

    Add new LiveMetrics data struct to proccess metrics

---
+++ client/src/data_structs/mod.rs
@@ -0,0 +1,31 @@
+pub struct LiveMetrics {
+    pub cpu: Cpu,
+}
+
+pub struct Cpu {
+    pub amount: usize,
+    pub freq: f32,
+    pub cpu_load: CpuLoad,
+}
+
+pub struct CpuLoad {
+    pub load_5m: f32,
+    pub load_10m: f32,
+    pub load_15m: f32,
+}
+
+pub fn init_live_metrics_struct() -> LiveMetrics {
+    let live_metrics_struct = LiveMetrics {
+        cpu: Cpu {
+            amount: 0,
+            freq: 0.,
+            cpu_load: CpuLoad {
+                load_5m: 0.,
+                load_10m: 0.,
+                load_15m: 0.,
+            },            
+        },
+    };
+
+    return live_metrics_struct;
+}
