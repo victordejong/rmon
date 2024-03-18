9a6766bb836b0865b34fd6e177164a7c09d93897
Victor de Jong <victor@victordejong.com>
2024-03-18 15:06:42 +0100

    Implement new memory collector in main.rs
    This commit contains the following changes:
     * Implement console logging of memory metrics
     * Move console logging to its separate subroutine
     * Bump rmon-client version to 0.1.1

--- client/src/main.rs
+++ client/src/main.rs
@@ -21,12 +21,24 @@ fn main() {
     let mut live_metrics: LiveMetrics = init_live_metrics_struct();
 
     loop {
-        live_metrics = collectors::cpu::collect_cpu_data(live_metrics);
+        live_metrics = collectors::cpu::collect(live_metrics);
+        live_metrics = collectors::mem::collect(live_metrics);
         
-        println!("CPU: cores: {}, frequency average {} MHz, load {} {} {}",
-            live_metrics.cpu.amount, live_metrics.cpu.freq, live_metrics.cpu.cpu_load.load_1m, live_metrics.cpu.cpu_load.load_5m,
-            live_metrics.cpu.cpu_load.load_15m);
+        print_to_console(&live_metrics);
 
         thread::sleep(SLEEP_DUR);
     }
+}
+
+fn print_to_console(live_metrics_struct: &LiveMetrics) {
+    print!("CPU: cores: {}, freq avg {} MHz, load {} {} {}; ",
+        live_metrics_struct.cpu.amount, live_metrics_struct.cpu.freq, live_metrics_struct.cpu.cpu_load.load_1m, live_metrics_struct.cpu.cpu_load.load_5m,
+        live_metrics_struct.cpu.cpu_load.load_15m);
+
+    if live_metrics_struct.mem.free > 10u64.pow(9) && live_metrics_struct.mem.cached > 10u64.pow(6) {
+        print!("MEM: free {} Gb, cached {} Mb; ", live_metrics_struct.mem.free as f32 / 1e9, live_metrics_struct.mem.cached as f32 / 1e6);
+    } else {
+        print!("MEM: free {} Kb, cached {} Kb; ", live_metrics_struct.mem.free, live_metrics_struct.mem.cached);
+    }
+    println!("");
 }
