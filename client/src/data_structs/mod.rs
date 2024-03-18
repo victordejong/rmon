9cfd2a64e5b683284243b19b4ffd0adaa4d817b8
Victor de Jong <victor@victordejong.com>
2024-03-18 14:03:55 +0100

    Adjust LiveMetrics data_structs to correctly match system load vars

--- client/src/data_structs/mod.rs
+++ client/src/data_structs/mod.rs
@@ -9,8 +9,8 @@ pub struct Cpu {
 }
 
 pub struct CpuLoad {
+    pub load_1m: f32,
     pub load_5m: f32,
-    pub load_10m: f32,
     pub load_15m: f32,
 }
 
@@ -20,8 +20,8 @@ pub fn init_live_metrics_struct() -> LiveMetrics {
             amount: 0,
             freq: 0.,
             cpu_load: CpuLoad {
+                load_1m: 0.,
                 load_5m: 0.,
-                load_10m: 0.,
                 load_15m: 0.,
             },            
         },
