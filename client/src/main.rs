7acd175d808612d6730904cbcae9afdd73d62448
Victor de Jong <victor@victordejong.com>
2024-03-18 18:56:55 +0100

    Implement new data structures into main.rs

--- client/src/main.rs
+++ client/src/main.rs
@@ -12,6 +12,7 @@ use collectors::{cpu, mem};
 
 mod data_structs;
 use data_structs::live_metrics::{LiveMetrics, init_live_metrics_struct};
+use data_structs::host_facts::{HostFacts, init_host_facts_struct};
 
 const SLEEP_DUR: time::Duration = time::Duration::from_millis(5000);
 
@@ -20,34 +21,38 @@ fn main() {
     println!("Getting CPU info with 5 second intervals...");
 
     let mut live_metrics: LiveMetrics = init_live_metrics_struct();
+    let host_facts: HostFacts = init_host_facts_struct();
 
     loop {
         live_metrics = cpu::collect(live_metrics);
         live_metrics = mem::collect(live_metrics);
         
-        print_to_console(&live_metrics);
+        print_to_console(&live_metrics, &host_facts);
 
         thread::sleep(SLEEP_DUR);
     }
 }
 
-fn print_to_console(live_metrics_struct: &LiveMetrics) {
+fn print_to_console(live_metrics_struct: &LiveMetrics, host_facts_struct: &HostFacts) {
 
     let mut cpu_freq_ext: String = "MHz".to_string();
     let cpu_freq: f32 = if live_metrics_struct.cpu.freq > 1024. { cpu_freq_ext = "GHz".to_string(); live_metrics_struct.cpu.freq / 1024. } else { live_metrics_struct.cpu.freq };
 
     print!("CPU: cores: {}, freq avg {} {}, load {} {} {}; ",
-        live_metrics_struct.cpu.amount, cpu_freq, cpu_freq_ext, live_metrics_struct.cpu.cpu_load.load_1m, live_metrics_struct.cpu.cpu_load.load_5m,
+        host_facts_struct.cpu.cores, cpu_freq, cpu_freq_ext, live_metrics_struct.cpu.cpu_load.load_1m, live_metrics_struct.cpu.cpu_load.load_5m,
         live_metrics_struct.cpu.cpu_load.load_15m);
 
 
-    let mem_free: u64 = live_metrics_struct.mem.free;
+    let mem_avail: u64 = live_metrics_struct.mem.available;
     let mem_cached: u64 = live_metrics_struct.mem.cached;
+    let mem_used: u64 = host_facts_struct.mem.ram_total - live_metrics_struct.mem.free - mem_cached;
 
-    if mem_free > 10u64.pow(9) && mem_cached > 10u64.pow(6) {
-        print!("MEM: free {} Gb, cached {} Mb; ", mem_free as f32 / 1e9, mem_cached as f32 / 1e6);
+    if mem_avail > 10u64.pow(9) && mem_cached > 10u64.pow(6) {
+        print!("MEM: used {} Gb, free {} Gb, cached {} Mb; ", mem_used as f32 / 1e9, mem_avail as f32 / 1e9, mem_cached as f32 / 1e6);
     } else {
-        print!("MEM: free {} Kb, cached {} Kb; ", mem_free, mem_cached);
+        print!("MEM: used {} Kb, free {} Kb, cached {} Kb; ", mem_used, mem_avail, mem_cached);
     }
     println!("");
+
+    return;
 }
