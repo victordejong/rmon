d91882876c224b5b6da0783f44ec0ae6831a106d
Victor de Jong <victor@victordejong.com>
2024-03-23 15:42:12 +0100

    Simplify disk collection loops for get_disk_stats

--- client/src/collectors/disk/linux.rs
+++ client/src/collectors/disk/linux.rs
@@ -10,15 +10,11 @@ pub fn get_disk_stats(live_metrics_struct: &mut LiveMetrics, disks: Vec<String>)
 
     // TODO: What the fuck?
     for system_disk in system_disks.iter() {
-        for disk in disks.iter() {
-            if &system_disk.name == disk {
-                for metric_disk in live_metrics_struct.disks.iter_mut() {
-                    if &metric_disk.name == disk {
-                        metric_disk.total_reads = system_disk.reads;
-                        metric_disk.total_writes = system_disk.writes;
-                        metric_disk.current_io = system_disk.in_progress;
-                    }
-                }
+        for config_disk in live_metrics_struct.disks.iter_mut() {
+            if system_disk.name == config_disk.name {
+                config_disk.total_reads = system_disk.reads;
+                config_disk.total_writes = system_disk.writes;
+                config_disk.current_io = system_disk.in_progress;
             }
         }
     }
