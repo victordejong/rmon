bf7a6eb0c5ecbfcfa621348af0daba5cbc16ac18
Victor de Jong <victor@victordejong.com>
2024-04-06 16:59:10 +0200

    Server: Use new HostFacts ProtoBuf message

--- server/src/main.rs
+++ server/src/main.rs
@@ -5,23 +5,50 @@ use live_metrics_protobuf::greeter_server::{Greeter, GreeterServer};
 use live_metrics_protobuf::{LiveMetricsMessage, LiveMetricsReply};
 use live_metrics_protobuf::live_metrics_message::{CpuMessage,MemMessage,SystemMessage};
 
+use host_facts_protobuf::host_facts_greeter_server::{HostFactsGreeter, HostFactsGreeterServer};
+use host_facts_protobuf::{HostFactsMessage, HostFactsReply};
+use host_facts_protobuf::host_facts_message::{HostCpuMessage,HostMemMessage,HostSystemMessage};
+
 pub mod live_metrics_protobuf {
     tonic::include_proto!("livemetrics");
 }
 
+pub mod host_facts_protobuf {
+    tonic::include_proto!("hostfacts");
+}
+
 #[derive(Debug, Default)]
-pub struct MyGreeter {}
+pub struct LiveMetricsGreeter {}
+#[derive(Debug, Default)]
+pub struct ServerHostFactsGreeter {}
 
 #[tonic::async_trait]
-impl Greeter for MyGreeter {
+impl Greeter for LiveMetricsGreeter {
     async fn send_live_metrics(
         &self,
         request: Request<LiveMetricsMessage>,
     ) -> Result<Response<LiveMetricsReply>, Status> {
         //println!("Got a request: {:?}", request);
-        print_received_host_facts(request);
+        //print_received_host_facts(request);
 
         let reply = live_metrics_protobuf::LiveMetricsReply {
+            success: true,
+        };
+
+        Ok(Response::new(reply))
+    }
+}
+
+#[tonic::async_trait]
+impl HostFactsGreeter for ServerHostFactsGreeter {
+    async fn send_host_facts(
+        &self,
+        request: Request<HostFactsMessage>,
+    ) -> Result<Response<HostFactsReply>, Status> {
+        //println!("Got a request: {:?}", request);
+        print_received_host_facts(request);
+
+        let reply = host_facts_protobuf::HostFactsReply {
             success: true,
         };
 
@@ -32,22 +59,24 @@ impl Greeter for MyGreeter {
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
     let addr = "0.0.0.0:50051".parse()?;
-    let greeter = MyGreeter::default();
+    let greeter = LiveMetricsGreeter::default();
+    let hostgreeter = ServerHostFactsGreeter::default();
 
     Server::builder()
         .add_service(GreeterServer::new(greeter))
+        .add_service(HostFactsGreeterServer::new(hostgreeter))
         .serve(addr)
         .await?;
 
     Ok(())
 }
 
-fn print_received_host_facts(request: Request<LiveMetricsMessage>) {
+fn print_received_host_facts(request: Request<HostFactsMessage>) {
 
-    let request_message: &LiveMetricsMessage = request.get_ref();
+    let request_message: &HostFactsMessage = request.get_ref();
     let remote_addr: SocketAddr = request.remote_addr().unwrap();
 
-    let system_message: &SystemMessage = match &request_message.system {
+    let system_message: &HostSystemMessage = match &request_message.system {
         None => {
             println!("ERROR: Malformed message received from {}!", remote_addr);
             return;
@@ -55,7 +84,7 @@ fn print_received_host_facts(request: Request<LiveMetricsMessage>) {
         Some(system) => system
     };
 
-    let cpu_message: &CpuMessage = match &request_message.cpu {
+    let cpu_message: &HostCpuMessage = match &request_message.cpu {
         None => {
             println!("ERROR: Malformed CpuMessage received from {} ({})!", system_message.hostname, remote_addr);
             return;
@@ -63,7 +92,7 @@ fn print_received_host_facts(request: Request<LiveMetricsMessage>) {
         Some(cpu) => cpu
     };
 
-    let mem_message: &MemMessage = match &request_message.mem {
+    let mem_message: &HostMemMessage = match &request_message.mem {
         None => {
             println!("ERROR: Malformed MemMessage received from {} ({})!", system_message.hostname, remote_addr);
             return;
