84c5df4608b3e8e5be7c34e428dc62c693933835
Victor de Jong <victor@victordejong.com>
2024-08-10 21:47:38 +0200

    Ansible: Refactor install script to work from a virtual environment

--- rmon-installer.sh
+++ rmon-installer.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+# shellcheck disable=SC1091
 
 set -eo pipefail
 
@@ -8,9 +9,31 @@ SCRIPT_WORKDIR="/tmp/rmon-installer"
 SRC_REMOTE="https://gitlab.com/victordejong/rmon.git"
 #############################
 
-# Check installer requirements
-SCRIPT_REQ="ansible ansible-playbook git"
-command -v ${SCRIPT_REQ} > /dev/null 2>&1
+
+######### FUNCTIONS #########
+ve() {
+    local py=${1:-python3.8}
+
+    # If not already in virtualenv
+    # $VIRTUAL_ENV is being set from $venv/bin/activate script
+	  if [ -z "${VIRTUAL_ENV}" ]; then
+        if [ ! -d "./.venv" ]; then
+            echo "Creating and activating virtual environment ./.venv"
+            ${py} -m venv "./.venv" --system-site-package
+            echo "export PYTHON=${py}" >> "./.venv/bin/activate"    # overwrite ${python} on .zshenv
+            source "./.venv/bin/activate"
+            echo "Upgrading pip"
+        else
+            echo "Virtual environment  ./.venv already exists, updating and activating..."
+            source "./.venv/bin/activate"
+        fi
+        ${py} -m pip install --upgrade pip
+        echo "$(pwd)"
+        pip install -U -r requirements.txt
+    else
+        echo "Already in a virtual environment!"
+    fi
+}
 
 script_help () {
     echo "
@@ -22,15 +45,27 @@ script_help () {
 
 run () {
     (mkdir -p "${SCRIPT_WORKDIR}" || true) && cd "${SCRIPT_WORKDIR}"
+
     (git clone "${SRC_REMOTE}" || true) && cd "${SCRIPT_WORKDIR}"/rmon/ansible
+
+    # Activate venv
+    ve python3
 
     # Run client-playbook.yaml or server-playbook.yaml
     ansible-playbook --connection=local -i localhost, -t "${1}" "${2}"-playbook.yaml
 
 }
+#############################
 
-# Validate arguments and run run()
+# Validate requirements, arguments and run run()
 main () {
+
+    # Check installer requirements
+    SCRIPT_REQ="python python3-venv git"
+
+    # shellcheck disable=SC2086
+    command -v ${SCRIPT_REQ} > /dev/null 2>&1
+
     case ${1} in
 
         "install" | "uninstall" | "install-src")
